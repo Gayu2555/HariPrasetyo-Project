f3a62459c54cbe8611c0096a4bbab4b8c44b0918
Gayu Yunma Ramadhan <gayuyunma1123@gmail.com>
2025-10-27 05:44:34 +0700

    Pembaruan masalah warna tema dan icon

--- lib/custom_theme.dart
+++ lib/custom_theme.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// Do not depend on Sizer in the theme definition; ThemeData may be
-// constructed before Sizer is initialized. Use fixed font sizes here.
 
 class CustomTheme {
   static final lightTheme = ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    primaryColor: const Color(0xff084f57),
-
+    primaryColor: const Color(0xfffe0000),
     textTheme: GoogleFonts.openSansTextTheme().apply(
       bodyColor: Colors.black87,
       displayColor: Colors.black87,
@@ -16,7 +13,7 @@ class CustomTheme {
       bodyColor: Colors.black87,
       displayColor: Colors.black87,
     ),
-    splashColor: const Color(0xff084f57),
-    iconTheme: const IconThemeData(color: Color(0xff084f57)),
+    splashColor: const Color(0xfffe0101),
+    iconTheme: const IconThemeData(color: Color(0xffff6b6b)),
   );
 }
